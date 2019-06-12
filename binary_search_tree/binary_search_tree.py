2411fa6b80d96e53bae199029fee1b7d795c77a0
James Basile <jbasile145@gmail.com>
2019-06-12 17:45:45 -0400

    insert test is passing

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -5,7 +5,26 @@ class BinarySearchTree:
     self.right = None
 
   def insert(self, value):
-    pass
+    new_node = BinarySearchTree(value)
+    curNode = self
+    while True:
+      # if cur node goes left
+      if curNode.value > new_node.value:
+        # if cur node exists
+        if curNode.left is not None:
+          #cur node becomes that value
+          curNode = curNode.left
+        else:
+          curNode.left = new_node
+          break
+      # if cur node goes right
+      if curNode.value < new_node.value:
+        if curNode.right is not None:
+          curNode = curNode.right
+        else:
+          curNode.right = new_node
+          break
+
 
   def contains(self, target):
     pass
@@ -15,3 +34,4 @@ class BinarySearchTree:
 
   def for_each(self, cb):
     pass
+
