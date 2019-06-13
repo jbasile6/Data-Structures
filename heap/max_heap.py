258ea3d3f7b886f7236a37ecfe6f04b46f847db3
James Basile <jbasile145@gmail.com>
2019-06-13 16:43:22 -0400

    all but delete working for max heap

--- heap/max_heap.py
+++ heap/max_heap.py
@@ -3,19 +3,36 @@ class Heap:
     self.storage = []
 
   def insert(self, value):
-    pass
+    self.storage.insert(len(self.storage), value)
+    self._bubble_up(len(self.storage) - 1)
 
   def delete(self):
-    pass
+    store = self.storage
+    store[0], store[len(store) - 1] = store[len(store) - 1], store[0]
+    m = store.pop(len(store) - 1)
+    self._sift_down(0)
+    return m
 
   def get_max(self):
-    pass
+    return self.storage[0]
 
   def get_size(self):
-    pass
+    return len(self.storage)
 
   def _bubble_up(self, index):
-    pass
+    store = self.storage
+    x = ((index- 1) // 2)
+    #stays in the index scope
+    if x >= 0:
+      if store[index] > store[x]:
+        store[index], store[x] = store[x], store[index]
+        self._bubble_up(x)
+      else:
+        return
+
+    else:
+      return
+
 
   def _sift_down(self, index):
     pass
